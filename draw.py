bc582d420665fd37f3e2e3ef4a9884cfa4918e04
Sawyer Greaves <u1239844@utah.edu>
2025-02-21 19:57:32 -0700

    Added the option to have arrowheads on the coordinate frames

--- draw.py
+++ draw.py
@@ -14,7 +14,7 @@ import forward_kinematics
 # DH params must be in the order d, a, alpha, theta
 # DH params should be in a 2d numpy array, each row being a joint
 # alpha and theta should be in radians, unless specified
-def draw(DH, deg=False):
+def draw(DH, deg=False, arrows=False):
     # Convert to floats if not already
     DH = DH / 1.0
     # Convert to radians if not already
@@ -28,18 +28,35 @@ def draw(DH, deg=False):
     ax = plt.figure().add_subplot(projection='3d')
     ax.plot(r[0], r[1], r[2], 'k')
     ax.axis('equal')
-    # Plot global coordinate frame
     num_joints = np.shape(DH)[0]
-    ax.plot([r[0, 0], r[0, 0] + 1], [r[1, 0], r[1, 0]], [r[2, 0], r[2, 0]], 'r', linewidth=5)
-    ax.plot([r[0, 0], r[0, 0]], [r[1, 0], r[1, 0] + 1], [r[2, 0], r[2, 0]], 'g', linewidth=5)
-    ax.plot([r[0, 0], r[0, 0]], [r[1, 0], r[1, 0]], [r[2, 0], r[2, 0] + 1], 'b', linewidth=5)
-    # Plot joint coordinate frame
-    for i in range(1, num_joints+1):
-        # Calculate local coordinate fram from rotation matrix
-        x = R[i-1] @ np.array([[1], [0], [0]])
-        y = R[i-1] @ np.array([[0], [1], [0]])
-        z = R[i-1] @ np.array([[0], [0], [1]])
-        # Plot local coordinate frame
-        ax.plot([r[0, i], r[0, i] + x[0, 0]], [r[1, i], r[1, i] + x[1, 0]], [r[2, i], r[2, i] + x[2, 0]], 'r', linewidth=5)
-        ax.plot([r[0, i], r[0, i] + y[0, 0]], [r[1, i], r[1, i] + y[1, 0]], [r[2, i], r[2, i] + y[2, 0]], 'g', linewidth=5)
-        ax.plot([r[0, i], r[0, i] + z[0, 0]], [r[1, i], r[1, i] + z[1, 0]], [r[2, i], r[2, i] + z[2, 0]], 'b', linewidth=5)
+    if arrows == True:
+        # Plot global coordinate frame
+        ax.quiver(r[0, 0], r[1, 0], r[2, 0], 1, 0, 0, color='r', linewidth=3)
+        ax.quiver(r[0, 0], r[1, 0], r[2, 0], 0, 1, 0, color='g', linewidth=3)
+        ax.quiver(r[0, 0], r[1, 0], r[2, 0], 0, 0, 1, color='b', linewidth=3)
+        # Plot joint coordinate frames
+        for i in range(1, num_joints+1):
+            # Calculate local coordinate fram from rotation matrix
+            x = R[i-1] @ np.array([[1], [0], [0]])
+            y = R[i-1] @ np.array([[0], [1], [0]])
+            z = R[i-1] @ np.array([[0], [0], [1]])
+            # Plot local coordinate frame
+            ax.quiver(r[0, i], r[1, i], r[2, i], x[0, 0], x[1, 0], x[2, 0], color='r', linewidth=3)
+            ax.quiver(r[0, i], r[1, i], r[2, i], y[0, 0], y[1, 0], y[2, 0], color='g', linewidth=3)
+            ax.quiver(r[0, i], r[1, i], r[2, i], z[0, 0], z[1, 0], z[2, 0], color='b', linewidth=3)
+    else:
+        # Plot global coordinate frame
+        ax.plot([r[0, 0], r[0, 0] + 1], [r[1, 0], r[1, 0]], [r[2, 0], r[2, 0]], 'r', linewidth=3)
+        ax.plot([r[0, 0], r[0, 0]], [r[1, 0], r[1, 0] + 1], [r[2, 0], r[2, 0]], 'g', linewidth=3)
+        ax.plot([r[0, 0], r[0, 0]], [r[1, 0], r[1, 0]], [r[2, 0], r[2, 0] + 1], 'b', linewidth=3)
+        # Plot joint coordinate frames
+        for i in range(1, num_joints+1):
+            # Calculate local coordinate fram from rotation matrix
+            x = R[i-1] @ np.array([[1], [0], [0]])
+            y = R[i-1] @ np.array([[0], [1], [0]])
+            z = R[i-1] @ np.array([[0], [0], [1]])
+            # Plot local coordinate frame
+            ax.plot([r[0, i], r[0, i] + x[0, 0]], [r[1, i], r[1, i] + x[1, 0]], [r[2, i], r[2, i] + x[2, 0]], 'r', linewidth=3)
+            ax.plot([r[0, i], r[0, i] + y[0, 0]], [r[1, i], r[1, i] + y[1, 0]], [r[2, i], r[2, i] + y[2, 0]], 'g', linewidth=3)
+            ax.plot([r[0, i], r[0, i] + z[0, 0]], [r[1, i], r[1, i] + z[1, 0]], [r[2, i], r[2, i] + z[2, 0]], 'b', linewidth=3)
+    
